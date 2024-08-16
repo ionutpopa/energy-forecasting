fb3e07159970a8ee753ceb404afedf17aae859e2
ionutpopa <ionut.popa9743@yahoo.com>
2024-08-16 12:36:11 +0300

    feat: change getData based on type of data

--- src/database/getData.ts
+++ src/database/getData.ts
@@ -1,33 +1,73 @@
-import { ElectricDataTable, sequelize } from "./config"
-import { EnergyDataType } from "../types/data"
+import { 
+    DataTypeEnum,
+    DataType
+} from "../types/data"
 import { Op } from "sequelize"
+import { getTable } from "../utils/getTable"
+import logger from "../utils/formatLogs"
 
-export const getData = async (
-    searchForConsumption: boolean,
-    searchForProduction: boolean
-) => {
-    const objectWithSearchParameters: { consumption?: any, production?: any } = {}
+type SearchParametersTypes = { 
+    consumption?: any, 
+    production?: any,
+    weather?: any
+}
 
-    if (searchForConsumption) {
-        objectWithSearchParameters.consumption = {
-            [Op.ne]: null
-        }
+const getDataParam = (typeOfData: DataTypeEnum) => {
+    let dataParam;
+    switch (typeOfData) {
+        case DataTypeEnum.CONSUMPTION:
+            dataParam = 'consumption'
+            break
+        case DataTypeEnum.GENERATION:
+            dataParam = 'generation'
+            break
+        case DataTypeEnum.WEATHER:
+            dataParam = 'averageTemperature'
+            break
+        case DataTypeEnum.POPULATION_GROWTH:
+            dataParam = 'population'
+            break
+        case DataTypeEnum.GDP_PER_CAPITA_GROWTH:
+            dataParam = 'gdpPerCapitaGrowth'
+            break
+        case DataTypeEnum.CO2_EMISSIONS:
+            dataParam = 'co2Emissions'
+            break
+        default:
+            break
     }
 
-    if (searchForProduction) {
-        objectWithSearchParameters.production = {
-            [Op.ne]: null
-        }
+    return dataParam;
+}
+
+export const getData = async (typeOfData: DataTypeEnum) => {
+    const table = getTable(typeOfData);
+    const dataParam = getDataParam(typeOfData) as string
+
+    if (!table) {
+        logger("Could not find table")
+        return undefined
+    }
+
+    if (!dataParam) {
+        logger("Could not get data param")
+        return undefined
+    }
+
+    let objectWithSearchParameters: SearchParametersTypes | any = {}
+
+    objectWithSearchParameters[dataParam] = {
+        [Op.ne]: null
     }
 
     // The find all function will return all the data that matches the search parameters
-    const data = await ElectricDataTable.findAll({
+    const data = await table.findAll({
         where: {
             ...objectWithSearchParameters
         }
     })
 
-    return (data as any).map((item: any) => {
+    return data.map((item) => {
         return item.toJSON()
-    }) as EnergyDataType[]
+    }) as DataType[]
 }
