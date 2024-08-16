b7e03bdcfbeadd5810df21caa7ea7eae57a84d8f
ionutpopa <ionut.popa9743@yahoo.com>
2024-08-16 12:35:54 +0300

    feat: changed getAllData to be more general based on type of data

--- src/database/getAllData.ts
+++ src/database/getAllData.ts
@@ -1,13 +1,50 @@
-import { ElectricDataTable } from "./config"
-import { EnergyDataType } from "../types/data"
+import { 
+    DataTypeEnum,
+    DataType
+} from "../types/data"
+import logger from "../utils/formatLogs";
+import { getTable } from "../utils/getTable";
 
-export const getAllData: () => Promise<EnergyDataType[]> = async () => {
+type GetAllDataReturn = {
+    data: DataType[];
+    totalRecords: number;
+    currentPage: number;
+    totalPages: number;
+}
+
+export const getAllData: (typeOfData: DataTypeEnum, page?: number, pageSize?: number, year?: number, sortOrder?: string) => Promise<(GetAllDataReturn | undefined)> = async (typeOfData: DataTypeEnum, page = 1, pageSize = 10, year = undefined, sortOrder = 'ASC') => {
+    const table = getTable(typeOfData);
+
+    const offset = (page - 1) * pageSize;
+    const whereCondition: any = {};
+
+    if (year !== null) {
+        whereCondition.year = year;
+    }
+
+    if (!table) {
+        logger("Could not find table")
+        return undefined
+    }
+
     try {
-        const findData = await ElectricDataTable.findAll()
+        const findData = await table.findAndCountAll({
+            where: whereCondition,
+            order: [['year', sortOrder]],
+            limit: pageSize,
+            offset: offset,
+        })
 
-        return findData.map((item: any) => item.toJSON()) as EnergyDataType[]
+        const returnObj = {
+            data: findData.rows.map((item: any) => item.toJSON()) as DataType[],
+            totalRecords: findData.count,
+            currentPage: page,
+            totalPages: Math.ceil(findData.count / pageSize),
+        }
+
+        return returnObj
     } catch (error) {
         console.error("An error occurred while getting all data", error)
-        return []
+        return undefined
     }
 }
