c8727559b140db3281716305800562f6ba4a1835
ionutpopa <ionut.popa9743@yahoo.com>
2024-09-04 22:18:24 +0300

    feat: separeted activation identifier type

---
+++ src/types/model.ts
@@ -0,0 +1,1 @@
+export type ActivationIdentifier = 'elu' | 'hardSigmoid' | 'linear' | 'relu' | 'relu6' | 'selu' | 'sigmoid' | 'softmax' | 'softplus' | 'softsign' | 'tanh' | 'swish' | 'mish' | 'gelu' | 'gelu_new';
