f7aa4bddd1890f68fc6214f66072547c12c71b7b
ionutpopa <ionut.popa9743@yahoo.com>
2024-09-09 15:26:45 +0300

    feat: added hidden layers to the model

--- src/linear-regression/build-model.ts
+++ src/linear-regression/build-model.ts
@@ -14,7 +14,15 @@ export const buildModel = (name: string, activationIdentifier: ActivationIdentif
     // Adds a layer instance on top of the layer stack.
     // no hidden layers, linear activation
     model.add(tf.layers.dense({ units: 1, inputShape: [1], activation: activationIdentifier }));
-    
+
+    // Add more hidden layers
+    model.add(tf.layers.dense({ units: 16, activation: 'relu' }));
+    model.add(tf.layers.dense({ units: 32, activation: 'relu' }));
+    model.add(tf.layers.dense({ units: 16, activation: 'relu' }));
+
+    // Add the output layer
+    model.add(tf.layers.dense({ units: 1, activation: activationIdentifier }));
+
     model.compile({
         optimizer: tf.train.adam(),
         loss: "meanSquaredError",
