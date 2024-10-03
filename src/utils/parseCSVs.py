81c8bd703e317ff4f47ccfe6ff674457c64255ae
ionutpopa <ionut.popa9743@yahoo.com>
2024-10-03 21:50:51 +0300

    wip: moved generation table creation on python side

--- src/utils/parseCSVs.py
+++ src/utils/parseCSVs.py
@@ -1,4 +1,3 @@
-import pandas
 import sqlite3
 import sys
 import json
@@ -14,19 +13,7 @@ class FilteredObject:
         for key, value in args.items():
             setattr(self, key, value)
 
-def filter_consumption_per_capita_data(data: Dict[list, Any]):
-    """
-    Filter unuseful consumption data
-    """
-    filtered_data = list() # this is a list
-    for country_data in data:
-        # check if first element exists, check if array is longer then 1, if first element is string, and if the third elemnt is not NaN
-        if len(country_data) >= 3:
-            if isinstance(country_data[0], str) and isinstance(int(country_data[2]), int) and not math.isnan(float(country_data[3])):
-                data_obj = dict(country=country_data[0], year=int(country_data[2]), consumption=float(country_data[3]))
-                filtered_data.append(data_obj)
-    return filtered_data
-
+# This function will be used for both consumption and consumption per capita data
 def filter_consumption_data(data: Dict[list, Any]):
     """
     Filter unuseful consumption data
@@ -40,9 +27,22 @@ def filter_consumption_data(data: Dict[list, Any]):
                 filtered_data.append(data_obj)
     return filtered_data
 
+def filter_generation_data(data: Dict[list, Any]):
+    """
+    Filter unuseful generation data
+    """
+    filtered_data = list() # this is a list
+    for country_data in data:
+        # check if first element exists, check if array is longer then 1, if first element is string, and if the third elemnt is not NaN
+        if len(country_data) >= 3:
+            if isinstance(country_data[0], str) and isinstance(int(country_data[2]), int) and not math.isnan(float(country_data[3])):
+                data_obj = dict(country=country_data[0], year=int(country_data[2]), generation=float(country_data[3]))
+                filtered_data.append(data_obj)
+    return filtered_data
+
 def process_csvs(table_name: TableType, cursor: sqlite3.Cursor) -> Dict[str, Any]:
     """
-    Processes CSV data provided as a JSON string and inserts it into a SQLite3 database.
+    Processes CSV data provided from the name of the table JSON file and inserts it into the SQLite3 database.
 
     :param data: JSON string representing CSV data.
     :param db_path: Path to the SQLite3 database.
@@ -57,7 +57,7 @@ def process_csvs(table_name: TableType, cursor: sqlite3.Cursor) -> Dict[str, Any
                 if json_file_path:
                     with open(json_file_path, 'r', encoding='utf-8') as json_data:
                         data = json.load(json_data)
-                        filtered_data = filter_consumption_per_capita_data(data)
+                        filtered_data = filter_consumption_data(data)
                         cursor.execute('''
                                     CREATE TABLE IF NOT EXISTS ElectricityConsumptionPerCapitaTable (
                                         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -73,16 +73,15 @@ def process_csvs(table_name: TableType, cursor: sqlite3.Cursor) -> Dict[str, Any
 
                         # Insert filtered data
                         for row in filtered_data:
-                            try:
-                                # Ensure the row has at least 3 elements to match table columns
-                                if len(row) >= 3:
+                            if len(row) >= 3:
+                                try:
                                     updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                                    cursor.execute(insert_query, (row[0], row[1], row[2], updated_at))
-                                    print('added', row[1])
-                            except sqlite3.IntegrityError as e:
-                                print(f"Error inserting row {row}: {e}")
+                                    cursor.execute(insert_query, (row['country'], row['year'], row['consumption'], updated_at))
+                                except sqlite3.IntegrityError as e:
+                                    print(f"Error inserting row {row}: {e}")
 
                         # Commit the transaction
+                        print("Populated ElectricityConsumptionPerCapitaTable")
                         conn.commit()
                     os.remove(json_file_path)
             case "ElectricityConsumptionTable":
@@ -106,17 +105,48 @@ def process_csvs(table_name: TableType, cursor: sqlite3.Cursor) -> Dict[str, Any
 
                         # Insert filtered data
                         for row in filtered_data:
-                            try:
-                                # Ensure the row has at least 3 elements to match table columns
-                                if len(row) >= 3:
+                            if len(row) >= 3:
+                                try:
                                     updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                                    cursor.execute(insert_query, (row[0], row[1], row[2], updated_at))
-                                    print('added', row[1])
-                            except sqlite3.IntegrityError as e:
-                                print(f"Error inserting row {row}: {e}")
+                                    cursor.execute(insert_query, (row['country'], row['year'], row['consumption'], updated_at))
+                                except sqlite3.IntegrityError as e:
+                                    print(f"Error inserting row {row}: {e}")
 
                         # Commit the transaction
                         conn.commit()
+                        print("Populated ElectricityConsumptionTable")
+                    os.remove(json_file_path)
+            case "ElectricityGenerationTable":
+                json_file_path = os.path.join(current_dir, "temp", "ElectricityGenerationTable.json")
+                if json_file_path:
+                    with open(json_file_path, 'r', encoding='utf-8') as json_data:
+                        data = json.load(json_data)
+                        filtered_data = filter_generation_data(data)
+                        cursor.execute('''
+                                    CREATE TABLE IF NOT EXISTS ElectricityGenerationTable (
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        country VARCHAR(255),
+                                        year INTEGER NOT NULL,
+                                        generation FLOAT,
+                                        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
+                                    )
+                                    ''')
+
+                        insert_query = 'INSERT INTO ElectricityGenerationTable (country, year, generation, updatedAt) VALUES (?, ?, ?, ?)'
+
+                        # Insert filtered data
+                        for row in filtered_data:
+                            if len(row) >= 3:
+                                try:
+                                    updated_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                                    cursor.execute(insert_query, (row['country'], row['year'], row['generation'], updated_at))
+                                except sqlite3.IntegrityError as e:
+                                    print(f"Error inserting row {row}: {e}")
+
+                        # Commit the transaction
+                        conn.commit()
+                        print("Populated ElectricityGenerationTable")
                     os.remove(json_file_path)
             case _:
                 print(f"An error occured during processing data for {table_name}")
--- src/utils/parseCSVs.ts
+++ src/utils/parseCSVs.ts
@@ -230,30 +230,49 @@ const callbackForCloseForConsumptionData = async () => {
 }
 
 const callbackForCloseForProductionData = async () => {
-  const arrayToStoreTransformedData = []
-
   // Remove first array because that's the row with the column names
-  productionData.shift()
+  consumptionPerCapitaData.shift()
+  try {
+    const tempFilePath = path.resolve("./src/utils/temp")
 
-  for (let i = 0; i < productionData?.length; i++) {
-    if (!productionData[i]?.[0] || !String(productionData[i]?.[0])) continue
-    if (isNaN(parseInt(productionData[i]?.[1]))) continue
-    const obj = {
-      country: productionData[i]?.[0],
-      year: productionData[i]?.[1],
-      generation: productionData[i]?.[2]
+    if (!fs.existsSync(tempFilePath)) {
+      await fs.promises.mkdir(tempFilePath, { recursive: true })
     }
 
-    arrayToStoreTransformedData.push(obj)
-  }
+    const jsonString = JSON.stringify(consumptionPerCapitaData, null, 2);
+    const pathToWrite = path.join(tempFilePath, `${ElectricityGenerationTable.name}.json`)
 
-  if (arrayToStoreTransformedData.length) {
-    // Store data in database
-    await ElectricityGenerationTable.bulkCreate(arrayToStoreTransformedData, {
-      updateOnDuplicate: ['year']
-    });
-    logger("Electricity Production Table Populated!")
+    await fs.promises.writeFile(pathToWrite, jsonString, { encoding: 'utf-8' })
+
+    const execRes = cp.execSync(`python ./src/utils/parseCSVs.py database.sqlite ${ElectricityGenerationTable.name}`, { cwd: process.cwd() })
+    logger(`stdout: ${execRes.toString()}`)
+  } catch (error) {
+    logger(`Error running python parsing: ${error}`, 'error')
   }
+  // const arrayToStoreTransformedData = []
+
+  // // Remove first array because that's the row with the column names
+  // productionData.shift()
+
+  // for (let i = 0; i < productionData?.length; i++) {
+  //   if (!productionData[i]?.[0] || !String(productionData[i]?.[0])) continue
+  //   if (isNaN(parseInt(productionData[i]?.[1]))) continue
+  //   const obj = {
+  //     country: productionData[i]?.[0],
+  //     year: productionData[i]?.[1],
+  //     generation: productionData[i]?.[2]
+  //   }
+
+  //   arrayToStoreTransformedData.push(obj)
+  // }
+
+  // if (arrayToStoreTransformedData.length) {
+  //   // Store data in database
+  //   await ElectricityGenerationTable.bulkCreate(arrayToStoreTransformedData, {
+  //     updateOnDuplicate: ['year']
+  //   });
+  //   logger("Electricity Production Table Populated!")
+  // }
 }
 
 const callbackForCloseWeatherData = async () => {
@@ -385,7 +404,7 @@ const callbackForCloseForCO2Emission = async () => {
 // We will use the readline module to read the file line by line and add it to the database
 createDynamicRL(energyConsumptionPerCapitaFilePath, callbackForLineForConsumptionPerCapitaData, callbackForCloseForConsumptionPerCapitaData);
 createDynamicRL(energyConsumptionFilePath, callbackForLineForConsumptionData, callbackForCloseForConsumptionData);
-// createDynamicRL(energyProductionFilePath, callbackForLineForProductionData, callbackForCloseForProductionData);
+createDynamicRL(energyProductionFilePath, callbackForLineForProductionData, callbackForCloseForProductionData);
 // createDynamicRL(dailyWeatherFilePath, callbackForLineForWeatherData, callbackForCloseWeatherData);
 // createDynamicRL(gdpPerCapitaFilePath, callbackForLineForGDPData, callbackForCloseForGdpPerCapitaData);
 // createDynamicRL(populationFilePath, callbackForLineForPopulationData, callbackForCloseForPopulationGrowth);
